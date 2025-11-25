474972b4fae096d68a84c7967ff3c299bf91f528
grzegorz switala <grz.switala@gmail.com>
2025-11-25 14:36:37 +0100

    Pętle - zadania

---
+++ petle_powtorzenie.py
@@ -0,0 +1,19 @@
+lista = [10, 56, 89, 59]
+
+#1. Chdzenie bezpośrednio po elementach listy
+#Do zmiennej b trafiają bezpośredio elementy listy, tzn. 10, 56, 89, 59
+'''for b in lista:
+    print(b)'''
+
+#2. Chodzenie po liście z użyciem indeksów
+#2.1. Co to jest indeks?
+# lista[2]
+# 2 - indeks
+# lista[2] - elementznajdujący się pod indeksem 2 = 89
+
+#2.2.
+'''for k in range(4): #range(0, 4)
+    print(lista[k])'''
+
+for k in range(len(lista)):
+    print(lista[k])
