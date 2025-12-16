2c4adcf97ca4a0656e968667644ecbf537cbdd17
grzegorz switala <grz.switala@gmail.com>
2025-12-16 14:35:38 +0100

    petla while zadania

--- petla_while.py
+++ petla_while.py
@@ -22,7 +22,7 @@ print(licznik)'''
 
 
 #Zadanie 2.
-popr_haslo = 'informatyka'
+'''popr_haslo = 'informatyka'
 haslo = input('Podaj haslo')
 proby = 1
 
@@ -35,4 +35,55 @@ if haslo == popr_haslo:
     print('Witaj w systemie mały nic nie znaczący człowieczku (:')
 
 else:
-    print('Lecz się na pamięć patafianie zaniedbany!!!!!, Nie wejdziesz tu (: (: (: naura!')
+    print('Lecz się na pamięć patafianie zaniedbany!!!!!, Nie wejdziesz tu (: (: (: naura!')'''
+
+
+#Zadanie 6.
+'''import random
+import time
+wynik1 = 0
+wynik2 = 0
+akcja = 0
+
+while not ((wynik1 >= 21 or wynik2 >= 21) and abs(wynik1 - wynik2) >= 2): #abs(x) = |x|
+    akcja += 1
+    print(f'Akcja {akcja}')
+    #syfek = int(input('Podaj numer wygranej drużyny'))
+    syfek = random.randint(1, 2)
+    if syfek == 1:
+        wynik1 += 1
+    if syfek == 2:
+        wynik2 += 1
+    print(f'Wynik {wynik1} : {wynik2}')
+    time.sleep(1)
+
+if wynik1 > wynik2:
+    print('Wygrała drużyna 1')
+else:
+    print('Wygrała drużyna 2')'''
+
+
+
+'''liczba = int(input('Podaj liczbę'))
+
+while liczba > 0:
+    cyfra = liczba % 10
+    liczba //= 10
+    print(cyfra, end = '')
+'''
+
+
+liczba = int(input('Co się lapmisz parówo dęta?! Ludzika z delmy nie widziałeś? A nie czekaj... Podaj liczbę: '))
+d = 2
+ile_czyn = 0
+ile_r_czyn = 0
+
+while liczba > 1:
+    if liczba % d == 0:
+        ile_r_czyn += 1
+    while liczba % d == 0:
+        liczba //= d
+        ile_czyn += 1
+    d += 1
+print(ile_czyn)
+print(ile_r_czyn)
