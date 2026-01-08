ffe8720b062776069078f1d45817365cc7ec141c
grzegorz switala <grz.switala@gmail.com>
2026-01-08 12:35:05 +0100

    listy

---
+++ listy.py
@@ -0,0 +1,39 @@
+#1) Listy a napisy
+napis = 'informatyka'
+
+
+#Zamiana napisu na listę
+lista = list(napis)
+print(lista)
+
+#Zakres i lista
+zakres = range(3, 10, 2)
+lista2 = list(zakres)
+print(lista2)
+
+
+#Indeksowanie elementów listy
+lista3 = ['osa', 99, 3.14, [5, 7, 8, 9]]
+print(lista3[1:3]) #wycinanie fragmentu listy
+print(lista3[3][2]) #obsługa listy zagnieżdżonej
+print(lista3[3][::2])
+
+#2) Powielanie listy
+lista4 = ['a', 45, 78]
+lista5 = [[4, 8], 56, 12]
+lista6 = lista5 + lista4
+print(lista6)
+
+#dodawanie list 2
+lista7 = ['a', 45, 78]
+lista8 = [[4, 8], 56, 12]
+lista7.extend(lista8)
+print(lista7)
+
+#"mnożenie" listy przez liczbę
+lista9 = [0] * 1000
+print(lista9)
+lista10 = [[0] * 10] * 10
+print(lista10)
+lista10[0][0] = 5
+print(lista10)
