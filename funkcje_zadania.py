3e9a7661bb461b39dc8f7c236da1ccadb78ae2cd
grzegorz switala <grz.switala@gmail.com>
2026-02-25 10:32:22 +0100

    funkcje

---
+++ funkcje_zadania.py
@@ -0,0 +1,22 @@
+#Zadanie 0.3.
+#a)
+def suma_v(u, v):
+    w = []
+    for i in range(len(u)):
+        suma = u[i] + v[i]
+        w.append(suma)
+    return w
+
+u = [2, 7, 3]
+v = [-1, 0, 4]
+
+wynik = suma_v(u, v)
+
+print(wynik)
+
+#b)
+def iloczyn(u, v):
+
+
+wynik = iloczyn(u, v)
+print(wynik)
